5ec353e35ef9785bf11dcc548749f938ef3241c8
Jared Nelson <jaredlnelson1@gmail.com>
2021-11-23 21:46:32 -0800

    Dashboard page
    Changed layout for some buttons
    Added notifications button
    Added clock to dashboard page
    Added event window for dashboard page
    Added imgs for empty space to dashboard page

--- client/src/pages/PageOverlay/TopButtons.js
+++ client/src/pages/PageOverlay/TopButtons.js
@@ -4,17 +4,24 @@ import {Link} from 'react-router-dom'
 import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
 import AddIcon from '@mui/icons-material/Add';
 import HomeIcon from '@mui/icons-material/Home';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 export default function TopButtons() {
     return (
         <div className="topWrapper">
-            <Link to="/settings" id="button"><SettingsSuggestIcon fontSize="large"/></Link>
+            <div className="settingsHomeWrapper">
+                <Link to="/settings" id="button"><SettingsSuggestIcon fontSize="large"/></Link>
+                <Link to="/" id="home"><HomeIcon fontSize="large"/></Link>
+            </div>
 
-            <Link to="/" id="home"><HomeIcon fontSize="large"/></Link>
-
-            <button id="button" onClick={() => console.log("Add button clicked!")}>
-                <AddIcon fontSize="large"/>
-            </button>
+            <div className="notificationsAddWrapper">
+                <button id="notificationButton" onClick={() => console.log("Notifications button clicked!")}>
+                    <NotificationsIcon fontSize="large"/>
+                </button>
+                <button id="addButton" onClick={() => console.log("Add button clicked!")}>
+                    <AddIcon fontSize="large"/>
+                </button>
+            </div>
         </div>
     )
 }
