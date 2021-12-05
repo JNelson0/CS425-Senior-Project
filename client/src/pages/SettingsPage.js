92b0b7807ca72060dd232b0fa9336c1087ae5217
Andrew <andrew.p.hervey@gmail.com>
2021-12-04 23:00:16 -0800

    Redid theme page, testing settings toggle

--- client/src/pages/SettingsPage.js
+++ client/src/pages/SettingsPage.js
@@ -17,18 +17,33 @@ var orange = true;
 export default function SettingsPage() {
   {console.log("darkmode: " + darkmode)}
 
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector(".press");
+    const sass = document.querySelector(".theme");
+    let currentTheme = sass.classList.contains("light") ? "light" : "dark";
+
+    button.addEventListener("click", function() {
+      currentTheme = currentTheme === "light" ? "dark" : "light";
+      {console.log(currentTheme)}
+    });
+  });
+  
   const [mode, setMode] = React.useState(false);
 
   return (
-    <div class="settingspage">
-      <TopButtons/>
-      <div className="middle"> 
-        <FormGroup>
-          {/* <Switch onClick(console.log(darkmode)) /> */}
-        </FormGroup>
-        <SettingsToggles/>
-      </div> 
-      <BottomBar/>
+    <div class ="settingspage">
+      <div class="theme light">
+        <TopButtons/>
+        <div className="middle"> 
+        <button type="button" class="button press"> Theme </button>
+          {/* <FormGroup>
+            <Switch onClick(console.log(darkmode)) />
+          </FormGroup> */}
+          <SettingsToggles/>
+        </div> 
+        <BottomBar/>
+      </div>
     </div>
   )
 }
