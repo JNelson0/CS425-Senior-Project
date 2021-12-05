4f014af837bbcf4c64b10cceae670859419a60b5
Alexander Mathew <amathew993@gmail.com>
2021-12-05 12:02:41 -0800

    base calendar still working on styling

--- client/src/pages/CalendarPage.js
+++ client/src/pages/CalendarPage.js
@@ -1,16 +1,29 @@
-import React from "react"
+import React, { useState } from 'react';
 import "./CalendarPage.scss"
 import BottomBar from "./PageOverlay/BottomBar.js"
+import Calendar from 'react-calendar';
 import TopButtons from "./PageOverlay/TopButtons.js"
 
+
+
 const CalendarPage = () => {
+const [value, setValue] = useState(new Date());
+function onChange(nextValue) {
+  setValue(nextValue);
+}
   return (
     <div className="calendar">
       <TopButtons/>
-      <div className="middle"> </div>
+      <div className="middle"> 
+        <Calendar
+            onChange={onChange}
+            value={value}/>
+            </div>
+              
       <BottomBar/>
     </div>    
   )
 }
+
 
 export default CalendarPage
