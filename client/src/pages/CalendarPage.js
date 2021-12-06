6fbeefbb02296517e1f6a6b3ef681b2a6f317a80
alexanderMathew <amathew993@gmail.com>
2021-12-05 17:32:46 -0800

    bit more styling

--- client/src/pages/CalendarPage.js
+++ client/src/pages/CalendarPage.js
@@ -1,22 +1,27 @@
-//import React, { useState } from 'react';
+import React, { useState } from 'react';
 import "./CalendarPage.scss"
 import BottomBar from "./PageOverlay/BottomBar.js"
 import Calendar from 'react-calendar';
 import TopButtons from "./PageOverlay/TopButtons.js"
 import 'react-calendar/dist/Calendar.css';
-import * as React from 'react';
+//import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import StaticDatePicker from '@mui/lab/StaticDatePicker';
 import EventContainer from "./EventContainer/EventContainer.js"
+import {useGlobalContext} from "../store"
+import data from "./DataTest.json"
+
 
 const CalendarPage = () => {
   const [value, setValue] = React.useState(new Date());
   function onChange(nextValue) {
     setValue(nextValue);
   }
+
     return (
+      
       <div className="calendar">
         <TopButtons/>
         <div className="middle"> 
