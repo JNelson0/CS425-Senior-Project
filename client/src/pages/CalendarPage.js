b80c0ee228e5f0914696d457c77ca06b1ea76d47
alexanderMathew <amathew993@gmail.com>
2021-12-05 19:50:39 -0800

    a much better looking calendar

--- client/src/pages/CalendarPage.js
+++ client/src/pages/CalendarPage.js
@@ -3,8 +3,6 @@ import "./CalendarPage.scss"
 import BottomBar from "./PageOverlay/BottomBar.js"
 import Calendar from 'react-calendar';
 import TopButtons from "./PageOverlay/TopButtons.js"
-//import 'react-calendar/dist/Calendar.css';
-//import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
@@ -12,7 +10,7 @@ import StaticDatePicker from '@mui/lab/StaticDatePicker';
 import EventContainer from "./EventContainer/EventContainer.js"
 import {useGlobalContext} from "../store"
 import data from "./DataTest.json"
-
+import RevoCalendar from 'revo-calendar'
 
 const CalendarPage = () => {
   const [value, setValue] = React.useState(new Date());
@@ -25,17 +23,12 @@ const CalendarPage = () => {
       <div className="calendar">
         <TopButtons/>
         <div className="middle"> 
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <StaticDatePicker
-              orientation="landscape"
-              openTo="day"
-              value={value}
-              onChange={(newValue) => {
-                setValue(newValue);
-              }}
-              renderInput={(params)=> <TextField {...params}/>}
-            />
-          </LocalizationProvider>
+        <RevoCalendar
+            style={{
+              borderRadius: "5px",
+              border: "5px solid #4F6995"
+              }}/>
+      
       <BottomBar/>
           </div>
         </div>
@@ -43,6 +36,7 @@ const CalendarPage = () => {
   
   
   } 
+
   
 export default CalendarPage
 
