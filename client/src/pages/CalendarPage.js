d4b6ea9ec6f872bae136e4bd9718ada8f1f66b88
alexanderMathew <amathew993@gmail.com>
2021-12-05 16:52:33 -0800

    more styled calendar

--- client/src/pages/CalendarPage.js
+++ client/src/pages/CalendarPage.js
@@ -1,30 +1,44 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 import "./CalendarPage.scss"
 import BottomBar from "./PageOverlay/BottomBar.js"
 import Calendar from 'react-calendar';
 import TopButtons from "./PageOverlay/TopButtons.js"
 import 'react-calendar/dist/Calendar.css';
-
-
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import StaticDatePicker from '@mui/lab/StaticDatePicker';
+import EventContainer from "./EventContainer/EventContainer.js"
 
 const CalendarPage = () => {
-const [value, setValue] = useState(new Date());
-function onChange(nextValue) {
-  setValue(nextValue);
-}
-  return (
-    <div className="calendar">
-      <TopButtons/>
-      <div className="middle"> 
-        <Calendar
-            onChange={onChange}
-            value={value}/>
-            </div>
-              
+  const [value, setValue] = React.useState(new Date());
+  function onChange(nextValue) {
+    setValue(nextValue);
+  }
+    return (
+      <div className="calendar">
+        <TopButtons/>
+        <div className="middle"> 
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <StaticDatePicker
+              orientation="landscape"
+              openTo="day"
+              value={value}
+              onChange={(newValue) => {
+                setValue(newValue);
+              }}
+              renderInput={(params)=> <TextField {...params}/>}
+            />
+          </LocalizationProvider>
       <BottomBar/>
-    </div>    
-  )
-}
-
-
+          </div>
+        </div>
+    );
+  
+  
+  } 
+  
 export default CalendarPage
+
+
