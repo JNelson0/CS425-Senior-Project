9244b076951cf0ed3e894308556d9d7f2b82b341
Jared Nelson <jaredlnelson1@gmail.com>
2022-01-27 17:02:36 -0800

    Fixed weird space with Overlay

--- client/src/pages/EventContainer/EventContainer.js
+++ client/src/pages/EventContainer/EventContainer.js
@@ -19,15 +19,16 @@ export default function EventContainer({id, setId, name, dateTime, darkmode}) {
     return (
         <div class={"theme " + (darkmode ? "light" : "dark")}>
             <li  className="eventbuttonWrapper">
-                <Link to={"/event"+id} className="eventbutton" onClick={() => setId(id)}>
-                    {console.log("/event")}
+            <Link to={"/event"+id} className="eventbutton" onClick={() => setId(id)}>
+                {console.log("/event")}
 
-                    <span>{id}</span>
-                    <span>{name}</span>
-                    <span>{dateTime}</span>
-                </Link>
-            </li>
+                <span>{id}</span>
+                <span>{name}</span>
+                <span>{dateTime}</span>
+            </Link>
             <DeleteForeverIcon className="trash" onClick={handleEventDelete} />
+         
+            </li>
         </div>
         
     )
