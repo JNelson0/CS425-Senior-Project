48b1712803aa7a90bb2a021c6b60ffcdb519d6b0
Andrew <andrew.p.hervey@gmail.com>
2022-01-30 22:41:25 -0800

    Slightly updated login
    not finished - but nicer looking

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,6 +2,9 @@ import React, {useEffect, useState} from "react"
 import {StandardLayout} from "../components"
 import {Navigate} from "react-router"
 import {useGlobalContext} from "../store"
+import "./LoginPage.scss"
+import { dividerClasses } from "@mui/material"
+import BackgroundIGM from "../img/university-background.png"
 
 const LoginPage = () => {
   const [emailOrUsername, setEmailOrUsername] = useState("")
@@ -43,26 +46,35 @@ const LoginPage = () => {
   }
 
   return (
-    <StandardLayout>
+    <div class = "loginpage">
+      <div class = "underlay">
+        <img id= "img" src={BackgroundIGM} alt="backg" />
+      </div>
+      <div class = "inputform">
       {error && <div>{error.message}</div>}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter email"
-          value={emailOrUsername}
-          onChange={handleEmailOrUsernameChange}
-        />
-        <input
-          type="password"
-          placeholder="Enter password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <button type="submit" disabled={loading}>
-          {loading ? "Loading..." : "Submit"}
-        </button>
-      </form>
-    </StandardLayout>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter email"
+            value={emailOrUsername}
+            onChange={handleEmailOrUsernameChange}
+          />
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <button type="submit" disabled={loading}>
+            {loading ? "Loading..." : "Submit"}
+          </button>
+        </form>
+      </div>
+
+      
+    </div>
+    // {error && <div>{error.message}</div>}
+    
   )
 }
 
