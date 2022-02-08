e6618d457e79687884f0f6085fe51bff24591aad
Jared Nelson <jaredlnelson1@gmail.com>
2022-02-07 21:22:10 -0800

    Revert "Merge branch 'master' of https://github.com/JNelson0/CS425-Senior-Project"
    This reverts commit 77a1cbff6e08c6c238ff6e70b0280d151d654edd, reversing
    changes made to 414b79fc2de240934ede6535341c85dc2c7292d8.

--- client/src/pages/DashboardPage.js
+++ client/src/pages/DashboardPage.js
@@ -6,46 +6,44 @@ import TopButtons from "./PageOverlay/TopButtons.js"
 import {useGlobalContext} from "../store"
 import EventContainer from "./EventContainer/EventContainer.js"
 import BackgroundImg from "../img/wolf.png"
-import Clock from "react-live-clock"
+import Clock from 'react-live-clock';
 import AddEvent from "./AddEvent/AddEvent.js"
+
 
 export default function DashboardPage({setId, darkmode}) {
   const {user, isLoggedIn, currentUserEventQuery} = useGlobalContext()
 
+  const [addOpen, setAddOpen] = useState(false);
+  
+  
   useEffect(() => {
-    if (isLoggedIn) {
+    if (isLoggedIn){
       currentUserEventQuery()
     }
   }, [])
-
+  
   return (
-    <div class={"theme " + (darkmode ? "light" : "dark")}>
+    <div class = {"theme " + (darkmode ? "light" : "dark")}>
       <div className="dashboard">
-        <TopButtons className="tb" addOpen={addOpen} setAddOpen={setAddOpen} />
-        <AddEvent addOpen={addOpen} setAddOpen={setAddOpen} />
+        <TopButtons className="tb" addOpen={addOpen} setAddOpen={setAddOpen}/>
+        <AddEvent addOpen={addOpen} setAddOpen={setAddOpen}/>
         <div className="listWrapper">
-          <div className="spacer">
+          <div className="spacer" >
             <img src={BackgroundImg} alt="Wolf" />
           </div>
           <div className="middleSpacer">
-            <Clock
-              className="clock"
-              format={"h:mm:ss a"}
-              ticking={true}
-              timezone={"US/Pacific"}
-            />
+            <Clock className="clock" format={'h:mm:ss a'} ticking={true} timezone={'US/Pacific'} />
             <ul>
               {user.loading? console.log("LOADING") : user.data.events.map(el =>(
                 <EventContainer key={user.data.events.findIndex(e => e === el)} setId={setId} id={el.id} name={el.title} dateTime={el.start} darkmode={darkmode}/> 
               ))}
-
             </ul>
           </div>
-          <div className="spacer">
+          <div className="spacer" >
             <img src={BackgroundImg} alt="Wolf" />
           </div>
         </div>
-        <BottomBar className="bb" />
+      <BottomBar className="bb" />
       </div>
     </div>
   )
