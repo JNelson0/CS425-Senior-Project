0ee5493da137e7db380081ffea0d32896f9d266d
Andrew <andrew.p.hervey@gmail.com>
2022-02-07 20:09:56 -0800

    Calendar merge

--- client/src/pages/CalendarPage.js
+++ client/src/pages/CalendarPage.js
@@ -1,37 +1,41 @@
 import React, { useState } from 'react';
 import "./CalendarPage.scss"
 import BottomBar from "./PageOverlay/BottomBar.js"
-import Calendar from 'react-calendar';
 import TopButtons from "./PageOverlay/TopButtons.js"
-import TextField from '@mui/material/TextField';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import StaticDatePicker from '@mui/lab/StaticDatePicker';
 import EventContainer from "./EventContainer/EventContainer.js"
 import {useGlobalContext} from "../store"
 //import data from "./DataTest.json"
 import RevoCalendar from 'revo-calendar'
+import * as ReactDOM from "react-dom";
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject } from '@syncfusion/ej2-react-schedule';
+import { render } from 'sass';
+
 
 const CalendarPage = ({darkmode}) => {
   const [value, setValue] = React.useState(new Date());
   function onChange(nextValue) {
     setValue(nextValue);
   }
+
   return (
     <div class={"theme " + (darkmode ? "light" : "dark")}>
       <div className="calendarPage">
         <TopButtons/>
         <div className="middle">
           <div class="calendar">
-            <RevoCalendar
-              style={{
-                borderRadius: "5px",
-                border: "5px solid #4F6995"
-                }}
-                allowAddEvent={true}
-                detailDateFormat =  "MM/DD/YYYY"
-                //addEvent = {addEvent}
-            />
+        {
+          <ScheduleComponent width='100%' height='550px'>
+            <link href="//cdn.syncfusion.com/ej2/ej2-base/styles/material.css" rel="stylesheet" />
+            <link href="//cdn.syncfusion.com/ej2/ej2-react-buttons/styles/material.css" rel="stylesheet" />
+            <link href="//cdn.syncfusion.com/ej2/ej2-react-calendars/styles/material.css" rel="stylesheet" />
+            <link href="//cdn.syncfusion.com/ej2/ej2-react-dropdowns/styles/material.css" rel="stylesheet" />
+            <link href="//cdn.syncfusion.com/ej2/ej2-react-inputs/styles/material.css" rel="stylesheet" />
+            <link href="//cdn.syncfusion.com/ej2/ej2-react-navigations/styles/material.css" rel="stylesheet" />
+            <link href="//cdn.syncfusion.com/ej2/ej2-react-popups/styles/material.css" rel="stylesheet" />
+            <link href="//cdn.syncfusion.com/ej2/ej2-react-schedule/styles/material.css" rel="stylesheet" />
+            <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+            <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
+          </ScheduleComponent>}
           </div>
         </div>
         <BottomBar/>
@@ -39,7 +43,4 @@ const CalendarPage = ({darkmode}) => {
     </div> 
   )
 }
-  
 export default CalendarPage
-
-
