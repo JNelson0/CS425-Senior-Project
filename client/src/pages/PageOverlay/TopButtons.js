18f5c82ea4c6e9b9879be60d4ad7f9e5e0ae8207
Andrew <andrew.p.hervey@gmail.com>
2022-02-14 19:28:38 -0800

    Font sizes fixed

--- client/src/pages/PageOverlay/TopButtons.js
+++ client/src/pages/PageOverlay/TopButtons.js
@@ -11,15 +11,15 @@ export default function TopButtons({addOpen, setAddOpen}) {
         <div className="topWrapper">
             <div className="settingsHomeWrapper">
                 <Link to="/settings" id="button"><SettingsIcon sx={{fontSize: 33}} /></Link>
-                <Link to="/" id="home"><HomeIcon fontSize="large"/></Link>
+                <Link to="/" id="home"><HomeIcon sx={{fontSize: 35}}/></Link>
             </div>
 
             <div className="notificationsAddWrapper">
                 <button id="notificationButton" onClick={() => console.log("Notifications button clicked!")}>
-                    <NotificationsIcon fontSize="large"/>
+                    <NotificationsIcon sx={{fontSize: 35}}/>
                 </button>
                 <button id="addButton" onClick={() => setAddOpen(!addOpen)}>
-                    <AddIcon fontSize="large"/>
+                    <AddIcon sx={{fontSize: 35}}/>
                 </button>
             </div>
         </div>
