2e80a7858add66af601511c8e435e41068c8f6f1
Jared Nelson <jaredlnelson1@gmail.com>
2022-02-14 20:43:08 -0800

    Calendar start

--- client/src/pages/CalendarPage.js
+++ client/src/pages/CalendarPage.js
@@ -7,6 +7,7 @@ import {useGlobalContext} from "../store"
 //import data from "./DataTest.json"
 import RevoCalendar from "revo-calendar"
 import * as ReactDOM from "react-dom"
+
 import {
     ScheduleComponent,
     Day,
@@ -19,66 +20,33 @@ import {
 import {render} from "sass"
 
 const CalendarPage = ({darkmode}) => {
-    const [value, setValue] = React.useState(new Date())
-    function onChange(nextValue) {
-        setValue(nextValue)
-    }
+    const {user} = useGlobalContext()
+    const list = []
 
     return (
         <div class={"theme " + (darkmode ? "light" : "dark")}>
             <div className="calendarPage">
                 <TopButtons />
+                {user.data.events.map(e => {
+                    list.push({
+                        Id: parseInt(e.id),
+                        Subject: e.title,
+                        StartTime: e.start,
+                        EndTime: e.finish,
+                    })
+                })}
                 <div className="middle">
                     <div class="calendar">
-                        {
-                            <ScheduleComponent width="100%" height="550px">
-                                <link
-                                    href="//cdn.syncfusion.com/ej2/ej2-base/styles/material.css"
-                                    rel="stylesheet"
-                                />
-                                <link
-                                    href="//cdn.syncfusion.com/ej2/ej2-react-buttons/styles/material.css"
-                                    rel="stylesheet"
-                                />
-                                <link
-                                    href="//cdn.syncfusion.com/ej2/ej2-react-calendars/styles/material.css"
-                                    rel="stylesheet"
-                                />
-                                <link
-                                    href="//cdn.syncfusion.com/ej2/ej2-react-dropdowns/styles/material.css"
-                                    rel="stylesheet"
-                                />
-                                <link
-                                    href="//cdn.syncfusion.com/ej2/ej2-react-inputs/styles/material.css"
-                                    rel="stylesheet"
-                                />
-                                <link
-                                    href="//cdn.syncfusion.com/ej2/ej2-react-navigations/styles/material.css"
-                                    rel="stylesheet"
-                                />
-                                <link
-                                    href="//cdn.syncfusion.com/ej2/ej2-react-popups/styles/material.css"
-                                    rel="stylesheet"
-                                />
-                                <link
-                                    href="//cdn.syncfusion.com/ej2/ej2-react-schedule/styles/material.css"
-                                    rel="stylesheet"
-                                />
-                                <link
-                                    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-                                    rel="stylesheet"
-                                />
-                                <Inject
-                                    services={[
-                                        Day,
-                                        Week,
-                                        WorkWeek,
-                                        Month,
-                                        Agenda,
-                                    ]}
-                                />
-                            </ScheduleComponent>
-                        }
+                        <ScheduleComponent
+                            width="100%"
+                            height="550px"
+                            selectedDate={new Date(2022, 1, 14)}
+                            eventSettings={{dataSource: list}}
+                        >
+                            <Inject
+                                services={[Day, Week, WorkWeek, Month, Agenda]}
+                            />
+                        </ScheduleComponent>
                     </div>
                 </div>
                 <BottomBar />
--- client/src/pages/PageOverlay/TopButtons.js
+++ client/src/pages/PageOverlay/TopButtons.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React from "react"
 import "./TopButtons.scss"
-import {Link} from 'react-router-dom'
-import SettingsIcon from '@mui/icons-material/Settings';
-import AddIcon from '@mui/icons-material/Add';
-import HomeIcon from '@mui/icons-material/Home';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import {Link} from "react-router-dom"
+import SettingsIcon from "@mui/icons-material/Settings"
+import AddIcon from "@mui/icons-material/Add"
+import HomeIcon from "@mui/icons-material/Home"
+import NotificationsIcon from "@mui/icons-material/Notifications"
 
 export default function TopButtons({addOpen, setAddOpen}) {
     return (
         <div className="topWrapper">
             <div className="settingsHomeWrapper">
-                <Link to="/settings" id="button"><SettingsIcon sx={{fontSize: 33}} /></Link>
-                <Link to="/" id="home"><HomeIcon fontSize="large"/></Link>
+                <Link to="/settings" id="button">
+                    <SettingsIcon sx={{fontSize: 33}} />
+                </Link>
+                <Link to="/" id="home">
+                    <HomeIcon fontSize="large" />
+                </Link>
             </div>
 
             <div className="notificationsAddWrapper">
-                <button id="notificationButton" onClick={() => console.log("Notifications button clicked!")}>
-                    <NotificationsIcon fontSize="large"/>
+                <button
+                    id="notificationButton"
+                    onClick={() => console.log("Notifications button clicked!")}
+                >
+                    <NotificationsIcon fontSize="large" />
                 </button>
                 <button id="addButton" onClick={() => setAddOpen(!addOpen)}>
-                    <AddIcon fontSize="large"/>
+                    <AddIcon fontSize="large" />
                 </button>
             </div>
         </div>
